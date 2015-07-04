7d879b641d4b59fe92569c99b75aa2fd747c0111
isman7 <isman7@gmail.com>
2015-07-04 10:55:59 +0200

    Better class definitions and methods

--- tgbot.py
+++ tgbot.py
@@ -22,48 +22,44 @@ class chat(object):
 class user(chat):
 
     def json(self):
-        
-        userjson = {'id': self.id,
-                    'first_name': self.first_name,
-                    'last_name': self.last_name,
-                    'username': self.username
-                    }  
-        
-        return userjson
+        return self.__dict__  
     
-    def __init__(self, userJson):
+    def __init__(self, **kwargs):
         chat.__init__(self)
-        self.json = userJson
-        self.id = self.json['id']
-        try:         
-            self.first_name = self.json['first_name']
-            self.last_name = self.json['last_name']
-            self.username = self.json['username']
+        try:
+            self.__dict__ = kwargs['userJson']
         except KeyError, e:
-            print e    
+            print e                
+            self.id = 0
+            self.first_name = u''
+            self.last_name = u''
+            self.username = u''
 
 class message(object):
     
-    def __init__(self, update):
+    def __init__(self, **kwargs):
         
         try:
-            
-            self.json = update['message'] 
+            update = kwargs['update'] 
+            self.__dict__ = update['message']            
+            self.userFrom = user(userJson = self.__dict__['from'])
+            if not 'title' in self.chat:
+                self.chat = user(userJson = self.chat)
+            else:
+                #self.chat = groupChat(self.chat)
+                print 'It is a group'
         except KeyError, e:
             print e
-            return None          
-            
-        self.date = self.json['date']
-        self.text = self.json['text']
-        self.userFrom = user(self.json['from'])
-        self.message_id = self.json['message_id']
-        self.chat = (self.json['chat'])
-        
-            
+            self.date = 0
+            self.text = u''
+            self.userFrom = user()
+            self.chatTo = chat()
+            self.message_id = u''
+            self.chat = chat()
             
 class bot(object):
     
-    def sendMessage(self, chat_id, text, ): 
+    def sendMessage(self, chat_id, text): 
         print chat_id
         return requests.get(self.url + u'sendMessage?chat_id=' + unicode(chat_id) + u'&text=' + text)
     
@@ -73,32 +69,23 @@ class bot(object):
             self.lastUpdates = requests.get(self.url + getUpdates + u'?offset=' + unicode(self.lastUpdateID + 1))
             self.lastUpdateID = self.lastUpdates.json()['result'][-1]['update_id']
             self.updatesBuff = self.updatesBuff + self.lastUpdates.json()['result']        
-            
-            
         except KeyError, e:
             print str(e)
-        
         except IndexError, e:
             print str(e)
         
         try: 
-            
             nextUpdate = self.updatesBuff[0]         
             self.updatesBuff = self.updatesBuff[1:]
-    
-                    
             return nextUpdate
-            
         except IndexError, e:
             print str(e)
             return None
-            
-        
+
     def __init__(self, path_to_key):
         
         keyFile = open(path_to_key, 'r')
         self.botKey = keyFile.readline().split('\n')[0]
-        
         self.url = u'https://api.telegram.org/bot' + unicode(self.botKey) + u'/'
         self.getMe = requests.get(self.url + getMe)
         
@@ -112,14 +99,10 @@ class bot(object):
         except KeyError, e:
             print 'A KeyError ocurred'
             print str(e)
-            
         except IndexError, e:
             self.lastUpdateID = 0
             self.updatesBuff = []
-            
             print e
             return None
-                
-            
-            
+
         keyFile.close()
