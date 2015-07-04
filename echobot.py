eaf5752993a91db207c6b6de3ce6f5eafbe1ab2f
isman7 <isman7@gmail.com>
2015-07-04 12:02:17 +0200

    Echo Bot example

--- echobot.py
+++ echobot.py
@@ -12,7 +12,7 @@ echoBot = tgbot.bot('ismansiete.txt')
 while True: 
     updateBot = echoBot.getUpdate()    
     if updateBot:
-        message = tgbot.message(update=updateBot)
+        message = tgbot.message(Update=updateBot)
         userFrom = message.userFrom
         echoBot.sendMessage(userFrom.id, message.text)       
         
